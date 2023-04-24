f9bb74d4f13b8abfce210d18f5ae764742d64add
Oleg Telegin <teleginos@mail.ru>
2023-04-24 22:20:29 +0300

    add solution task15

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (0 < number && number <=5)
+{
+    Console.WriteLine("Будний день, иди работай!");
+}
+
+else if (number == 6 || number == 7)
+{
+    Console.WriteLine("Выходной! сегодня можешь отдохнуть...");
+}
+
+else
+{
+    Console.WriteLine("Неправильный ввод дынных!");
+}
