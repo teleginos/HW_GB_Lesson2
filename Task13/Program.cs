ea06fdd74a8ef4cd5aa19a0f2d93d0f75e6645f2
Oleg Telegin <teleginos@mail.ru>
2023-04-24 22:12:24 +0300

    add solution task13

---
+++ Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+string numberstr = Console.ReadLine();
+int count = 1;
+
+if (numberstr.Length > 2)
+{
+    count += numberstr.Length - 3;
+    int number = Convert.ToInt32(numberstr);
+    int number2 =Convert.ToInt32(Math.Pow(10, count));
+    Console.WriteLine($"Третья цифра числа {number} = {(number % number2) / (number2 / 10)}");
+}
+
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
