10902e53a8120c7aed9848b71c8e28400a4569a1
Oleg Telegin <teleginos@mail.ru>
2023-04-24 21:41:28 +0300

    add solution task10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трех значное число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (100 < number && number < 1000)
+{
+    Console.WriteLine($"Вторая цифра числа {number} = {number % 100 / 10}");
+}
+
+else
+{
+    Console.WriteLine("Неправильный ввод данных!");
+}
